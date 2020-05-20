bb5291e632dbbd6f885b6cb719ac766ecb3d8a84
Halomaple <jeff.doyle@foxmail.com>
2020-05-20 11:08:11 +0800

    Reorder and clean up

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,48 +1,45 @@
 Import-Module MyModule
 
-New-Alias -name vs -value Start-VS -description "Launch visual studio" -option ReadOnly
-New-Alias -name kvs -value Start-KillVS -description "Kill visual studio" -option ReadOnly
-New-Alias -name rvs -value Start-RestartVS -description "Restart visual studio" -option ReadOnly
-New-Alias -name kis -value Start-KillIISExpress -description "Kill IIS Express" -option ReadOnly
-New-Alias -name cis -value Start-ClearIISExpressCache -description "Clear IIS Express Cache" -option ReadOnly
-New-Alias -name sql -value Start-SSMS -description "Launch sql server management studio" -option ReadOnly
-
+## Folders
 New-Alias -name p -value Enter-ProjectsFolder -description "Projects Folder" -option ReadOnly
 New-Alias -name psf -value Enter-PowerShellFolder -description "My PowerShell Folder" -option ReadOnly
 
+## Logs
 New-Alias -name ev -value Start-EventViewer -description "Open Event Viewer" -option ReadOnly
 New-Alias -name log -value Start-LoggingEvents -description "Logging Events" -option ReadOnly
-
-New-Alias -name clean -value Start-Clean -description "Clean IIS" -option ReadOnly
 New-Alias -name clearlog -value Start-ClearEventLogs -description "Clear Event Logs" -option ReadOnly
-New-Alias -name k -value Start-KillProcess -description "Kill process" -option ReadOnly
 
+
+## VMs
 New-Alias -name ubuntu -value Start-LogonUbuntu -description "Logon Ubuntu machine" -option ReadOnly
 New-Alias -name centos -value Start-LogonCentOS -description "Logon CentOS machine" -option ReadOnly
+
+## Network
 New-Alias -name ab -value Start-ConnectToWorkNetwork -description "Work network wifi" -option ReadOnly
 New-Alias -name sz -value Start-ConnectToInternet -description "Internet wifi" -option ReadOnly
 New-Alias -name wifi -value Start-ShowCurrentWifiNetwork -description "Show current wifi" -option ReadOnly
-New-Alias -name rst -value Start-RestartComputer -description "Restart computer" -option ReadOnly
-New-Alias -name stc -value Start-ShutdownComputer -description "Shutdown computer" -option ReadOnly
-New-Alias -name hibernate -value Start-HibernateComputer -description "Hibernate computer" -option ReadOnly
 
+## Sites
+New-Alias -name azure -value New-AzurePortal -description "Azure Portal" -option ReadOnly
 New-Alias -name gitlab -value New-Gitlab -description "Gitlab" -option ReadOnly
-New-Alias -name id -value New-WorkItem -description "Work item" -option ReadOnly
 New-Alias -name github -value New-Github -description "Github" -option ReadOnly
 New-Alias -name mail -value New-Mail -description "Mail" -option ReadOnly
-
 New-Alias -name open -value New-OpenUrlInBrowser -description "Open url in browser" -option ReadOnly
-New-Alias -name dict -value New-YoudaoDict -description "Youdao dict" -option ReadOnly
-New-Alias -name can -value New-CanIUse -description "Baidu" -option ReadOnly
 
+## Search
 New-Alias -name baidu -value New-Baidu -description "Baidu" -option ReadOnly
 New-Alias -name bing -value New-Bing -description "Bing" -option ReadOnly
+New-Alias -name can -value New-CanIUse -description "Baidu" -option ReadOnly
+New-Alias -name dict -value New-YoudaoDict -description "Youdao dict" -option ReadOnly
+New-Alias -name id -value New-WorkItem -description "Work item" -option ReadOnly
 New-Alias -name g -value New-Google -description "Google" -option ReadOnly
 New-Alias -name s -value New-StackOverflow -description "StackOverflow" -option ReadOnly
-New-Alias -name msdn -value New-MSDN -description "MSDN" -option ReadOnly
 
-New-Alias -name clipc -value Start-ClipCurrentPath -description "Clip Current Path" -option ReadOnly
-New-Alias -name ll -value Start-ListItems -description "List items" -option ReadOnly
-
+## Misc
 New-Alias -name commands -value Start-ShowCommands -description "Show Commands" -option ReadOnly
-New-Alias -name update -value Update-MyModule -description "Update Module" -option ReadOnly
+New-Alias -name clipc -value Start-ClipCurrentPath -description "Clip Current Path" -option ReadOnly
+New-Alias -name k -value Start-KillProcess -description "Kill process" -option ReadOnly
+New-Alias -name ll -value Start-ListItems -description "List items" -option ReadOnly
+New-Alias -name hibernate -value Start-HibernateComputer -description "Hibernate computer" -option ReadOnly
+New-Alias -name rst -value Start-RestartComputer -description "Restart computer" -option ReadOnly
+New-Alias -name stc -value Start-ShutdownComputer -description "Shutdown computer" -option ReadOnly
--- Modules/MyMOdule/MyModule.psm1
+++ Modules/MyMOdule/MyModule.psm1
@@ -3,152 +3,61 @@ $PowerShellFolder = "~\Documents\WindowsPowerShell"
 $LocalIP = "$Env:LocalIP"
 $UbuntuMachine = "$Env:UbuntuMachine"
 $CentOSMachine = "$Env:CentOSMachine"
-
-$VS = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe"
-$VSProcessName = "devenv"
-$IISExpressProcessName = "iisexpress"
-$SSMS = "C:\Program Files (x86)\Microsoft SQL Server\140\Tools\Binn\ManagementStudio\Ssms.exe"
 $Chrome = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
 
 function Start-Up {
     Write-Host "
     Good to see you!
 
-    Programs:
-        vs - 'Launch visual studio'
-        kvs - 'Kill visual studio'
-        rvs - 'Restart visual studio'
-        sql - 'Luanch sql server management studio'
-
     Folders:
         p - 'Projects folder'
         psf - 'My PowerShell Folder'
 
-    Bat:
-
-    Projects:
-        kis - 'Kill IIS Express'
-        cis - 'Clear IIS Express Cache'
-        clean - 'Clean IIS'
-        clearlog - 'Clear Event Logs'
-        k - 'Kill process'
-
-    Websites:
-        gitlab - 'Gitlab'
-        id - 'Work item'
-        github - 'Github'
-        mail - 'Mail'
-
-    Tools:
-        open [url] - 'Open url in browser'
-        dict [word] - 'Youdao dict'
-        can [word] - 'Can I Use'
-        clipc - 'Clip Current Path'
-        ll - 'List items'
+    Logs:
         ev - 'Open Event Viewer'
         log - 'Logging Events'
+        clearlog - 'Clear Event Logs'
 
-    Computer:
-        ubuntu - 'Logon Ubuntu machine'
+    VMs:
         centos - 'Logon CentOS machine'
+        ubuntu - 'Logon Ubuntu machine'
+
+    Network:
         ab - 'Connect to work network'
         sz - 'Connect to internet'
         wifi - 'Show current wifi'
-        rst - 'Restart computer'
-        stc - 'Shutdown computer'
-        hibernate - 'Hibernate computer'
+
+    Sites:
+        azure - 'Azure Portal'
+        github - 'Github'
+        gitlab - 'Gitlab'
+        mail - 'Mail'
+        open [url] - 'Open url in browser'
 
     Search:
         baidu [keywords] - 'Search keywords using Baidu'
         bing [keywords] - 'Search keywords using Bing'
+        can [word] - 'Can I Use'
+        dict [word] - 'Youdao dict'
+        id - 'Work item'
         g [keywords] - 'Search keywords using Google'
         s [keywords] - 'Search keywords usering StackOverflow'
-        msdn - 'MSDN'
 
-    PS:
+    Misc:
         commands - 'Show Commands'
-        update - 'Update Module'
+        clipc - 'Clip Current Path'
+        k - 'Kill process'
+        ll - 'List items'
+        hibernate - 'Hibernate computer'
+        rst - 'Restart computer'
+        stc - 'Shutdown computer'
     "
 }
 
 Start-Up
 
-function Start-ShowCommands {
-    Start-Up
-}
-
-function Update-MyModule {
-    Remove-Module MyModule
-    Import-Module MyModule
-
-    Write-Host "Module updated!"
-}
-
-function Start-VS {
-    Start-Process $VS
-}
-
-function Start-KillVS {
-    $process = Get-VSProcess
-    if ($process.Id) {
-        Stop-Process $process.Id
-        Write-Host "Visual Studio is terminated"
-    }
-    else {
-        Write-Host "Visual Studio has not started"
-    }
-}
-
-function Get-VSProcess {
-    return Get-Process $VSProcessName
-}
-
-function Start-RestartVS {
-    Start-KillVS
-    Start-VS
-}
-
-function Get-IISExpressProcess {
-    return Get-Process $IISExpressProcessName
-}
-
-function Start-KillIISExpress {
-    $process = Get-IISExpressProcess
-    if ($process.Id) {
-        Stop-Process $process.Id
-        Write-Host "IIS Express is terminated"
-    }
-    else {
-        Write-Host "IIS Express has not started"
-    }
-}
-
-function Start-ClearIISExpressCache {
-    Write-Host "Clearing IISExpress Cache..."
-    Remove-Item ~/Documents/IISExpress/* -r
-    Write-Host "IISExpress Cache Cleaned!"
-}
-
-function Start-Clean () {
-    Start-KillIISExpress
-    Start-ClearIISExpressCache
-}
-
-function Start-ClearEventLogs () {
-    & Clear-EventLog "Application"
-    & Clear-EventLog "Security"
-    & Clear-EventLog "System"
-}
-
-function Start-KillProcess () {
-    & Stop-Process -Name $args[0]
-}
-
-
-function Start-SSMS {
-    Start-Process $SSMS
-}
-
+## Folders
 function Enter-ProjectsFolder {
     Set-Location $ProjectsFolder
 }
@@ -157,26 +66,33 @@ function Enter-PowerShellFolder {
     Set-Location $PowerShellFolder
 }
 
-function Get-VpnStatus {
-    rasdial
-}
 
+## Logs
 function Start-EventViewer {
     & 'eventvwr'
 }
 
 function Start-LoggingEvents {
-    get-eventlog -LogName Application -Newest $args[0] -Source *$args[1]* | Select-Object Index, EntryType, InstanceId, Message | format-list
+    Get-Eventlog -LogName Application -Newest $args[0] -Source *$args[1]* | Select-Object Index, EntryType, InstanceId, Message | format-list
+}
+
+function Start-ClearEventLogs () {
+    & Clear-EventLog "Application"
+    & Clear-EventLog "Security"
+    & Clear-EventLog "System"
+}
+
+## VMs
+function  Start-LogonCentOS {
+    ssh root@$CentOSMachine
 }
 
 function  Start-LogonUbuntu {
     ssh ubuntu@$UbuntuMachine
 }
 
-function  Start-LogonCentOS {
-    ssh root@$CentOSMachine
-}
 
+## Network
 function Start-ConnectToWorkNetwork {
     $currentWifi = Start-ShowCurrentWifiNetwork
     if ($currentWifi -match "$Env:WorkNetworkName") {
@@ -228,28 +144,11 @@ function Start-ShowCurrentWifiNetwork {
     return netsh wlan show interfaces | Select-String '\sSSID'
 }
 
+## Sites
 function New-AzurePortal {
     $url = "https://portal.azure.com/"
     & $Chrome $url
     Write-Host "Azure Portal opened in Chrome."
-}
-
-function New-OpenUrlInBrowser {
-    $url = "http://$($args[0])"
-    Write-Host "Opened $url in browser"
-    & $Chrome $url
-}
-
-function New-Gitlab {
-    $url1 = $Env:GitlabPath
-    & $Chrome $url1
-    Write-Host "Gitlab opened in Chrome."
-}
-
-function New-WorkItem {
-    $url = $Env:JiraPath + $args[0]
-    & $Chrome $url
-    Write-Host "Workd item opened in Chrome."
 }
 
 function New-Github {
@@ -258,24 +157,26 @@ function New-Github {
     Write-Host "Github opened in Chrome."
 }
 
+function New-Gitlab {
+    $url1 = $Env:GitlabPath
+    & $Chrome $url1
+    Write-Host "Gitlab opened in Chrome."
+}
+
 function New-Mail {
     $url = "https://outlook.office.com/mail/inbox"
     & $Chrome $url
     Write-Host "Mail opened in Chrome."
 }
 
-function New-YoudaoDict {
-    Write-Host "Looking up $($args[0]) in Youdao dict online"
-    $url = "http://dict.youdao.com/w/eng/$($args[0])"
+function New-OpenUrlInBrowser {
+    $url = "http://$($args[0])"
+    Write-Host "Opened $url in browser"
     & $Chrome $url
 }
 
-function New-CanIUse {
-    Write-Host "Can I use $($args[0]) ?"
-    $url = "http://caniuse.com/#search=$($args[0])"
-    & $Chrome $url
-}
 
+## Search
 function New-Baidu {
     Write-Host "Searched keywords using Baidu."
     $url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=$args"
@@ -286,6 +187,24 @@ function New-Bing {
     Write-Host "Searched keywords using Bing."
     $url = "http://cn.bing.com/search?q=$args"
     & $Chrome $url
+}
+
+function New-CanIUse {
+    Write-Host "Can I use $($args[0]) ?"
+    $url = "http://caniuse.com/#search=$($args[0])"
+    & $Chrome $url
+}
+
+function New-YoudaoDict {
+    Write-Host "Looking up $($args[0]) in Youdao dict online"
+    $url = "http://dict.youdao.com/w/eng/$($args[0])"
+    & $Chrome $url
+}
+
+function New-WorkItem {
+    $url = $Env:JiraPath + $args[0]
+    & $Chrome $url
+    Write-Host "Workd item opened in Chrome."
 }
 
 function New-Google {
@@ -300,20 +219,27 @@ function New-StackOverflow {
     Write-Host "StackOverflow opened in Chrome."
 }
 
-function New-MSDN {
-    $url = "https://msdn.microsoft.com/en-us/"
-    & $Chrome $url
-    Write-Host "MSDN opened in Chrome."
+
+## Misc
+function Start-ShowCommands {
+    Start-Up
 }
 
 function Start-ClipCurrentPath {
     $pwd.Path | clip
 }
 
+function Start-KillProcess () {
+    & Stop-Process -Name $args[0]
+}
+
 function Start-ListItems {
     & Get-ChildItem
 }
 
+function Start-HibernateComputer {
+    & shutdown /h
+}
 function Start-RestartComputer {
     if ($args[0]) {
         & shutdown /r /t $args[0]
@@ -330,8 +256,4 @@ function Start-ShutdownComputer {
     else {
         & shutdown /s /t 0
     }
-}
-
-function Start-HibernateComputer {
-    & shutdown /h
 }
